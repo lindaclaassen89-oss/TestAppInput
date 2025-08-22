2044ea57cc7c94421027b64f1323b3f3f65041ce
lindaclaassen89-oss <lindaclaassen89@gmail.com>
2025-08-22 10:24:07 +0200

    Update main.py Logging and f strings

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import streamlit as st
 from datetime import datetime
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.info(f"Current time1:{datetime.now()}")
 
 st.write("Current time:", datetime.now())
-print("Current time:", datetime.now())
+print(f"Current time2:{datetime.now()}")
